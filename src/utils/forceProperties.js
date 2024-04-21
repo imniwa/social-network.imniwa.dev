c9e9daa80e7ebd1b5c35f76d987ba6a55c8df88b
niwa <niwaisme@proton.me>
2024-04-21 23:58:54 +0700

    add: deafult forceProperties

---
+++ src/utils/forceProperties.js
@@ -0,0 +1,33 @@
+export const defaultForceProperties = {
+  center: {
+    x: 0.5,
+    y: 0.5,
+  },
+  charge: {
+    enabled: true,
+    strength: -30,
+    distanceMin: 1,
+    distanceMax: 2000,
+  },
+  collide: {
+    enabled: true,
+    strength: 0.7,
+    iterations: 1,
+    radius: 5,
+  },
+  forceX: {
+    enabled: true,
+    strength: 0.1,
+    x: 0.5,
+  },
+  forceY: {
+    enabled: true,
+    strength: 0.1,
+    y: 0.5,
+  },
+  link: {
+    enabled: true,
+    distance: 150,
+    iterations: 1,
+  },
+};
