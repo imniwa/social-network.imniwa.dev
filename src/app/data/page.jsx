3108386758e2fab4d5cbac53f2ebc2134ea29428
niwa <niwaisme@proton.me>
2024-05-02 16:27:23 +0700

    feat: add back2home button

--- src/app/data/page.jsx
+++ src/app/data/page.jsx
@@ -1,15 +1,27 @@
+import clsx from "clsx";
 import dynamic from "next/dynamic";
 
 export const metadata = {
   title: "Data used in this project",
 };
 
-const NodeDetails = dynamic(() => import('@/components/NodeDetails'))
-const DataTable = dynamic(() => import('@/components/DataTable'))
+const NodeDetails = dynamic(() => import("@/components/NodeDetails"));
+const DataTable = dynamic(() => import("@/components/DataTable"));
 
 export default function Page() {
   return (
     <div className="container mx-auto flex flex-col gap-4 py-8">
+      <div className="border border-slate-200 p-4 rounded-md bg-slate-50">
+        <a
+          className={clsx([
+            "border border-slate-200 bg-slate-50 text-slate-400 px-4 py-2 rounded-md",
+            "hover:bg-white hover:text-slate-800"
+          ])}
+          href="/"
+        >
+          Back to visualization
+        </a>
+      </div>
       <NodeDetails />
       <DataTable />
     </div>
