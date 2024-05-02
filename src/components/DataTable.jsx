88e4515e139067c1e00e76f2d1df2a718e580099
niwa <niwaisme@proton.me>
2024-05-02 16:22:01 +0700

    feat: add filtering with usesearchparams

--- src/components/DataTable.jsx
+++ src/components/DataTable.jsx
@@ -1,12 +1,23 @@
 "use client";
 import tweet from "@/data/tweet.json";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { clsx } from "clsx";
+import { useSearchParams } from "next/navigation";
 
 export default function DataTable() {
   const RANGE = 10;
+  const searchParams = useSearchParams();
   const [iteration, setIteration] = useState(0);
+  const [filtered, setFiltered] = useState(false);
+  const username = searchParams.get("username");
   const { data } = tweet;
+
+  useEffect(() => {
+    setFiltered(typeof username === "string");
+    return () => {
+      setFiltered(false);
+    };
+  }, [filtered, username]);
 
   const handleNext = () => {
     if (iteration >= Math.ceil(data.length / RANGE)) return;
@@ -31,51 +42,61 @@ export default function DataTable() {
           </tr>
         </thead>
         <tbody>
-          {data.slice(iteration*RANGE, iteration*RANGE+RANGE).map((e, i) => {
-            return (
-              <tr key={i}>
-                <td className="border border-slate-200 text-center">{i + (10*iteration) + 1}</td>
-                <td className="border border-slate-200 text-center">
-                  {e.username}
-                </td>
-                <td className="border border-slate-200 text-center">
-                  {e.mention}
-                </td>
-                <td className="border border-slate-200">{e.raw_content}</td>
-                <td className="border border-slate-200">
-                  <table className="table-auto w-full border-collapse border border-slate-200">
-                    <thead>
-                      <tr>
-                        <th className="border border-slate-200">Token</th>
-                        <th className="border border-slate-200">Score</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {e.tf_idf
-                        .sort((a, b) => b.score - a.score)
-                        .map((e, i) => {
-                          return (
-                            <tr key={i}>
-                              <td className="border border-slate-200 text-center">
-                                {e.token}
-                              </td>
-                              <td className="border border-slate-200 text-center">
-                                {e.score.toFixed(4)}
-                              </td>
-                            </tr>
-                          );
-                        })}
-                    </tbody>
-                  </table>
-                </td>
-              </tr>
-            );
-          })}
+          {data
+            .filter((e) =>
+              filtered
+                ? e.username === username || e.mention === username
+                : true
+            )
+            .slice(iteration * RANGE, iteration * RANGE + RANGE)
+            .map((e, i) => {
+              return (
+                <tr key={i}>
+                  <td className="border border-slate-200 text-center">
+                    {i + 10 * iteration + 1}
+                  </td>
+                  <td className="border border-slate-200 text-center">
+                    {e.username}
+                  </td>
+                  <td className="border border-slate-200 text-center">
+                    {e.mention}
+                  </td>
+                  <td className="border border-slate-200 p-4">{e.raw_content}</td>
+                  <td className="border border-slate-200">
+                    <table className="table-auto w-full border-collapse border border-slate-200">
+                      <thead>
+                        <tr>
+                          <th className="border border-slate-200">Token</th>
+                          <th className="border border-slate-200">Score</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {e.tf_idf
+                          .sort((a, b) => b.score - a.score)
+                          .map((e, i) => {
+                            return (
+                              <tr key={i}>
+                                <td className="border border-slate-200 text-center">
+                                  {e.token}
+                                </td>
+                                <td className="border border-slate-200 text-center">
+                                  {e.score.toFixed(4)}
+                                </td>
+                              </tr>
+                            );
+                          })}
+                      </tbody>
+                    </table>
+                  </td>
+                </tr>
+              );
+            })}
         </tbody>
       </table>
       <div className="mt-4 flex flex-row-reverse">
         <div className="flex gap-4">
-          <button onClick={handlePrevious}
+          <button
+            onClick={handlePrevious}
             className={clsx([
               "text-slate-400 bg-slate-50 border border-slate-400 rounded-sm px-4 py-2",
               "hover:text-slate-800 hover:bg-white transition-all",
@@ -83,7 +104,8 @@ export default function DataTable() {
           >
             Previous
           </button>
-          <button onClick={handleNext}
+          <button
+            onClick={handleNext}
             className={clsx([
               "text-slate-400 bg-slate-50 border border-slate-400 rounded-sm px-4 py-2",
               "hover:text-slate-800 hover:bg-white transition-all",
